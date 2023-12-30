35588f6a598917ba675d6a0447f7d2abbadf1186
Makalaster <perlow.sam@gmail.com>
2023-12-30 13:59:42 -0500

    Increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.makalaster.etherealdialpad"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.3"
+        versionCode = 2
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
