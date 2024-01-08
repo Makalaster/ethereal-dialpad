2447ce281db7951334c135ad519f20ec35008ecb
Makalaster <perlow.sam@gmail.com>
2024-01-08 11:03:06 -0500

    Increment version. Add privacy policy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.makalaster.etherealdialpad"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.10"
+        versionCode = 5
+        versionName = "1.0.0-RC"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
